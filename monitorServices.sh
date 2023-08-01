b7d0a96a0f1393896422708dad76cd49fc52c0f1
gowdashb <102322324+gowdashb@users.noreply.github.com>
2023-08-01 13:59:19 +0530

    Update monitorServices.sh

--- monitorServices.sh
+++ monitorServices.sh
@@ -1,9 +1,10 @@
+"""
 Question: Write a Bash script to monitor specific services and automatically restart them if they are found 
 to be stopped. The services to monitor are sshd and jenkins. The script should periodically check if these 
 services are running, and if any of them are not running, it should send an email notification and restart 
 the stopped service using systemctl
 ------------------------------------------------------------------------------------------------------------
-
+"""
 #!/bin/bash
 
 services="sshd jenkins"
